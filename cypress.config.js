f36bc886c2e33e1b1bd6dfbdfca6e46ce5e8c980
Carlos Mestas <carlos93esp@gmail.com>
2023-10-21 22:32:04 +0200

    Añadidos.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'junit',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
